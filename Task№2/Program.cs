50e591aee095d279b49fede37b73e81fe2a2715c
Polina <polina_shamova@mail.ru>
2023-02-11 18:45:15 +0300

    add Task№2

---
+++ Task№2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(" Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+if(numberB > numberA)
+{
+Console.Write("Первое число меньше второго");
+}
+else
+{
+if(numberB == numberA)
+{
+Console.Write("Первое число равно второму");
+}
+
+else
+{
+Console.Write("Первое число больше второго");
+}
+}
+
+
+
