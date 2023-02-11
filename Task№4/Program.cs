041527f86747c7a993b3fdea03df77641f5c35b7
Polina <polina_shamova@mail.ru>
2023-02-11 19:31:20 +0300

    add Task№4

---
+++ Task№4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(" Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите третье число");
+int numberC = int.Parse(Console.ReadLine());
+
+if((numberA > numberB) && (numberA > numberC))
+{
+Console.Write(numberA);
+}
+else
+{
+if(numberB > numberC)
+{
+Console.Write(numberB);
+}
+
+else
+{
+Console.Write(numberC);
+}
+}
