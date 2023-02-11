df6ea00dbeb22a6939b23a9aa8702b9389f235e9
Polina <polina_shamova@mail.ru>
2023-02-11 19:44:32 +0300

    add Task№6

---
+++ Task№6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+if(numberA %2 == 0)
+{
+Console.Write("четное");
+}
+else
+{ 
+Console.Write("Нечетное");
+}
