c86cdd0fc3748281269d1013dfc3e631cd605f48
Polina <polina_shamova@mail.ru>
2023-02-14 23:58:19 +0300

    add Task№13

---
+++ Task№13/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = new Random().Next(10, 1000);
+Console.WriteLine(num);
+int A = num/100;
+int B = num%10;
+if(A != 0)
+{
+    Console.WriteLine(B);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
