7ce241dad1ae719f713470d0adb60f57dc0205fa
Polina <polina_shamova@mail.ru>
2023-02-18 00:26:48 +0300

    add Task№19_

---
+++ Task№19_/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ввести пятизначное число: ");
+int N = int.Parse(Console.ReadLine()!);
+if(N/10000 == N%10 & N%10000/1000 == N%100/10)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
