b818d42461781a18ea3b4dc42b0d198385681fc0
Polina <polina_shamova@mail.ru>
2023-02-22 00:05:51 +0300

    add Task №25

---
+++ Task№25/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("ввести первое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("ввести второе число: ");
+int B = int.Parse(Console.ReadLine()!);
+
+int product = 1;
+int number = 1;
+for(number=1; number <= B; number++)
+     {
+         product = product * A;
+     }    
+Console.WriteLine($"Произведение чисел = {product}");
