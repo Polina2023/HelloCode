f91d88629543bbdd157b8a8feca6d29546e4cff2
Polina <polina_shamova@mail.ru>
2023-02-22 00:41:39 +0300

    add Task№27

---
+++ Task№27/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNumbers (int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+       result += number % 10;
+        number = number /10;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write($"Сумма цифр в числе: {SumNumbers(number)}");
+
+
